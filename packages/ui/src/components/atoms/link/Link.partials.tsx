21f6783f6dfffcd9ba7ca67c3415094a61122a41
Pius Prince <45078705+piusprince@users.noreply.github.com>
2024-10-24 16:48:02 +0000

    feat(AWR-77): add link variants

--- packages/ui/src/components/atoms/link/Link.partials.tsx
+++ packages/ui/src/components/atoms/link/Link.partials.tsx
@@ -1,6 +1,29 @@
 import React from 'react'
 import { LinkProps } from './Link.type'
 import NextLink from 'next/link'
+import { cva, VariantProps, cx } from 'class-variance-authority'
+import { ArrowNorthEast } from '../icons/ArrowNorthEast'
+
+const LinkVariants = cva(
+  [
+    'text-primary font-semibold hover:text-primary-700 hover:underline focus:text-primary-700 visited:bg-primary-400 disabled:text-primary-200 group'
+  ],
+  {
+    variants: {
+      variant: {
+        internal: [''],
+        external: ['flex items-center gap-1'],
+        button: ['flex gap-1 p-10 border-secondary border-2  rounded-sm']
+      }
+    },
+    compoundVariants: [{}],
+    defaultVariants: {
+      variant: 'internal'
+    }
+  }
+)
+
+export type LinkVariantsProps = VariantProps<typeof LinkVariants>
 
 export const ExternalLink = ({
   href,
@@ -8,9 +31,10 @@ export const ExternalLink = ({
   title,
   children,
   color,
-  iconLeft,
+  iconLeft = false,
   className,
   style,
+  variant,
   ...props
 }: LinkProps) => {
   return (
@@ -18,20 +42,33 @@ export const ExternalLink = ({
       href={href}
       target={target}
       title={title}
-      className={className}
+      className={cx(LinkVariants({ variant }), className)}
       style={style}
       {...props}
     >
-      {children}
+      <span
+        className={`flex items-center flex-row gap-1 ${iconLeft ? 'flex-row-reverse' : ''}`}
+      >
+        {children}
+        <ArrowNorthEast className="w-[14px] h-[14px] transition-colors group-hover:text-primary-700" />
+      </span>
     </a>
   )
 }
 
-export const InternalLink = ({ href, children, ...props }: LinkProps) => {
+export const InternalLink = ({
+  href = '',
+  variant,
+  children,
+  ...props
+}: LinkProps) => {
   return (
-    // <NextLink href={href} {...rest}>
-    //   {children}
-    // </NextLink>
-    <></>
+    <NextLink
+      href={href}
+      className={cx(LinkVariants({ variant }), props.className)}
+      {...props}
+    >
+      {children}
+    </NextLink>
   )
 }
