9a6955648b717d7105167cd0031d326df4f41a8b
Pius Prince <45078705+piusprince@users.noreply.github.com>
2024-10-24 16:49:17 +0000

    chore(AWR-77): add type

--- packages/ui/src/components/atoms/link/Link.type.ts
+++ packages/ui/src/components/atoms/link/Link.type.ts
@@ -1,14 +1,16 @@
 import { ComponentProps } from 'react'
+import { LinkVariantsProps } from './Link.partials'
 
 export enum TargetProps {
   BLANK = '_blank',
   SELF = '_self',
   PARENT = '_parent',
-  TOP = '_top',
+  TOP = '_top'
 }
 
 export type LinkProps = {
   iconLeft?: boolean
   external?: boolean
   label?: string
-} & ComponentProps<'a'>
+} & ComponentProps<'a'> &
+  LinkVariantsProps
