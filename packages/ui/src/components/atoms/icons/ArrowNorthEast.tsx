8ea6d28ac5903c87249071dd3dd5b74906ff8619
Pius Prince <45078705+piusprince@users.noreply.github.com>
2024-10-24 16:47:06 +0000

    feat(AWR-77): add arrow svg component

---
+++ packages/ui/src/components/atoms/icons/ArrowNorthEast.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+type ArrowNorthEastProps = {
+  className?: string
+}
+
+export const ArrowNorthEast = ({
+  className
+}: ArrowNorthEastProps): React.ReactElement => (
+  <svg
+    className={`text-primary  ${className}`}
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+  >
+    <g clip-path="url(#clip0_147_13751)">
+      <path
+        d="M9 5V7H15.59L4 18.59L5.41 20L17 8.41V15H19V5H9Z"
+        fill="currentColor"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_147_13751">
+        <rect width="24" height="24" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+)
