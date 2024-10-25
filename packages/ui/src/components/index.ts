bbfe4d10db580e371d080a9b657762c4eacc4d42
Pius Prince <45078705+piusprince@users.noreply.github.com>
2024-10-25 11:47:03 +0000

    chore(AWR-77): export link component

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,6 +1,7 @@
 // components
 export * from './atoms/button/Button'
-
+export * from './atoms/link/Link'
 
 // type
 export * from './atoms/button/Button.types'
+export * from './atoms/link/Link.type'
