9915842ae8771a69a135109d3cd2234029aeb154
Pius Prince <45078705+piusprince@users.noreply.github.com>
2024-10-25 08:10:44 +0000

    chore(AWR-77): add style to icon

--- packages/ui/src/components/atoms/icons/ArrowNorthEast.tsx
+++ packages/ui/src/components/atoms/icons/ArrowNorthEast.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 
 type ArrowNorthEastProps = {
   className?: string
+  style?: React.CSSProperties
 }
 
 export const ArrowNorthEast = ({
-  className
+  className,
+  style
 }: ArrowNorthEastProps): React.ReactElement => (
   <svg
     className={`text-primary  ${className}`}
+    style={style}
     xmlns="http://www.w3.org/2000/svg"
     width="24"
     height="24"
