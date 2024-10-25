1c448b4d3155cf05f001c67879ea9b8027308fce
Pius Prince <45078705+piusprince@users.noreply.github.com>
2024-10-25 14:38:11 +0000

    fix(AWR-77): change svg clippath naming

--- packages/ui/src/components/atoms/icons/ArrowNorthEast.tsx
+++ packages/ui/src/components/atoms/icons/ArrowNorthEast.tsx
@@ -18,7 +18,7 @@ export const ArrowNorthEast = ({
     viewBox="0 0 24 24"
     fill="none"
   >
-    <g clip-path="url(#clip0_147_13751)">
+    <g clipPath="url(#clip0_147_13751)">
       <path
         d="M9 5V7H15.59L4 18.59L5.41 20L17 8.41V15H19V5H9Z"
         fill="currentColor"
