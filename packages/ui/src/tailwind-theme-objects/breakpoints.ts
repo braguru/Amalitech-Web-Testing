867a0c8a9f8ebc95ef9b5ba5dfd78292d4f22609
Justice Uzoma Ajah <77506031+J-Ajah@users.noreply.github.com>
2024-11-05 08:13:25 +0000

    fix: updated breakpoints to use pixel equivalent values (#40)

--- packages/ui/src/tailwind-theme-objects/breakpoints.ts
+++ packages/ui/src/tailwind-theme-objects/breakpoints.ts
@@ -1,7 +1,9 @@
 export const breakpoints = {
-  xs: '20rem',
-  sm: '36rem',
-  md: '48rem',
-  lg: '85.375rem'
+  xs: '320px',  // Equivalent to 20rem
+  sm: '576px',  // Equivalent to 36rem
+  md: '768px',  // Equivalent to 48rem
+  lg: '1024px', // Equivalent to 64rem
+  xl: '1280px', // Equivalent to 80rem
+  '2xl': '1536px', // Equivalent to 96rem
   // add extra screen sizes here
 }
