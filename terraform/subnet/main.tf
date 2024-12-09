3960b6e9b8e2e32fb006de3d3a309b4cceeeea08
PrinceOforiAnkamah <sabastainofori@gmail.com>
2024-12-09 12:23:16 +0000

    chore: add terraform template

---
+++ terraform/subnet/main.tf
@@ -0,0 +1,10 @@
+resource "aws_subnet" "Amalitech-Website-Subnet" {
+  vpc_id                  = var.vpc_id
+  cidr_block              = var.cidr_block
+  map_public_ip_on_launch = var.map_public_ip_on_launch
+
+  tags = {
+    Name = var.subnet_name
+  }
+}
+
---
+++ terraform/vpc/main.tf
@@ -0,0 +1,10 @@
+resource "aws_vpc" "internal-projects-vpc" {
+  cidr_block           = var.cidr_block
+  enable_dns_hostnames = var.enable_dns_hostnames
+  enable_dns_support   = var.enable_dns_support
+
+  tags = {
+    name = var.vpc_name
+  }
+
+}
